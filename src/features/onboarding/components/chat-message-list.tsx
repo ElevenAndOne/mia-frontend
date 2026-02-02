bfd5b035eead6cb51599b64119a915ca42c720cb
Craig Chihururu <craigchihururu@gmail.com>
2026-02-02 20:47:52 +0200

    Update onboarding components and color tokens

--- src/features/onboarding/components/chat-message-list.tsx
+++ src/features/onboarding/components/chat-message-list.tsx
@@ -13,7 +13,7 @@ interface ChatMessageListProps {
 }
 
 export const ChatMessageList = ({ messages, isTyping, isStreaming, onChoiceSelect, endRef}: ChatMessageListProps) => (
-  <div className="flex-1 h-full overflow-y-auto px-4 pt-6 pb-6 safe-bottom">
+  <div className="flex-1 h-full overflow-y-auto px-4 pb-6">
     <div className="flex flex-col gap-3 min-h-full max-w-3xl mx-auto w-full">
       {/* Spacer pushes messages to bottom when content is short */}
       <div className="flex-1" />
